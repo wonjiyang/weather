2b8de8e849fccaa33216401ee771a8feee07e571
wonjiyang <ywj010827@naver.com>
2025-10-26 01:55:07 +0900

    second

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import WeatherBox from './component/WeatherBox';
 import WeatherBtn from './component/WeatherBtn';
+import { ClipLoader } from 'react-spinners';
 
 function App() {
   const [weather, setWeather] = useState(null);
+  const [city, setCity] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [apiError, setAPIError] = useState('');
+  const cities = ['Paris', 'New York', 'Tokyo', 'Incheon'];
+
   const getCurrentLocation = () => {
     navigator.geolocation.getCurrentPosition((position) => {
       let lat = position.coords.latitude;
@@ -13,21 +19,68 @@ function App() {
       getWeatherByCurrentLocation(lat, lon);
     });
   };
+
   const getWeatherByCurrentLocation = async (lat, lon) => {
-    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=d38da50cf19274f0b22db2bb34bf9fa4&units=metric`;
-    let response = await fetch(url);
-    let data = await response.json();
-    setWeather(data);
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=d38da50cf19274f0b22db2bb34bf9fa4&units=metric`;
+      setLoading(true);
+      let response = await fetch(url);
+      let data = await response.json();
+      setWeather(data);
+      setLoading(false);
+    } catch (error) {
+      setAPIError(err.message);
+      setLoading(false);
+    }
   };
+
+  const getWeatherByCity = async () => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d38da50cf19274f0b22db2bb34bf9fa4&units=metric`;
+      setLoading(true);
+      let response = await fetch(url);
+      let data = await response.json();
+      setWeather(data);
+      setLoading(false);
+    } catch (error) {
+      setAPIError(error.message);
+      setLoading(false);
+    }
+  };
+
+  const changeCurrent = (city) => {
+    if (city === 'current') {
+      setCity('');
+    } else {
+      setCity(city);
+    }
+  };
+
   useEffect(() => {
-    getCurrentLocation();
-  }, []);
+    if (city === '') {
+      getCurrentLocation();
+    } else {
+      getWeatherByCity();
+    }
+  }, [city]);
+
   return (
     <div>
-      <div className="container">
-        <WeatherBox weather={weather} />
-        <WeatherBtn />
-      </div>
+      {loading ? (
+        <ClipLoader color="#f5f5f5" loading={loading} size={150} />
+      ) : !apiError ? (
+        <div className="container">
+          <WeatherBox weather={weather} />
+          <WeatherBtn
+            cities={cities}
+            setCity={setCity}
+            changeCurrent={changeCurrent}
+            city={city}
+          />
+        </div>
+      ) : (
+        apiError
+      )}
     </div>
   );
 }
--- src/component/WeatherBtn.jsx
+++ src/component/WeatherBtn.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 
-const WeatherBtn = () => {
+const WeatherBtn = ({ cities, setCity, changeCurrent, city }) => {
   return (
     <div className="weather-btn">
-      <Button variant="warning" className="btn">
+      <Button
+        variant={city === '' ? 'warning' : 'outline-warning'}
+        className="btn"
+        onClick={() => changeCurrent('current')}
+      >
         Current Location
       </Button>
-      <Button variant="warning">paris</Button>
-      <Button variant="warning">new york</Button>
+      {cities.map((item, index) => (
+        <Button
+          key={index}
+          variant={city === item ? 'warning' : 'outline-warning'}
+          className="btn"
+          onClick={() => changeCurrent(item)}
+        >
+          {item}
+        </Button>
+      ))}
     </div>
   );
 };
